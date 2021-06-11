f64dc77137b7edffe45439da7ffd115ef83ee12e
victoremmanuelch <85482257+victoremmanuelch@users.noreply.github.com>
2021-06-11 22:33:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute(){
+
+    var principal = document.getElementById("principal").value;
+    if (principal<=0){
+      document.getElementById("result").innerHTML = "Please enter a valid amount.";
+      return;
+    }
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = (parseInt(principal) * parseFloat(rate) * parseInt(years))/100;
+    var currentDate = new Date();
+    var year = currentDate.getFullYear();
+    var maturityYear = parseInt(years) + parseInt(year);
+    var maturityValue = parseInt(interest) + parseInt(principal);
+    var output = "If you deposit " + principal +"<br>at an interest rate of " + rate +"%.<br>You will receive an amount of " + maturityValue + "<br> at year " + maturityYear + ".<br>";
+    document.getElementById("result").innerHTML = output;
 }
-        
+function displayRate(){
+    var rate = document.getElementById("rate").value;
+    rate += "%";
+    document.getElementById("displayRate").innerHTML = rate;
+}      
